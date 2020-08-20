c9ccd69e60dbb9f6924fdb38395970938386aad1
Turaki01 <ezekiel_zakari@yahoo.com>
2020-08-20 19:52:55 +0100

    navbar css fix

--- src/components/clients.js
+++ src/components/clients.js
@@ -1,12 +1,8 @@
 import React from 'react'
 
 const Client = props => (
-    <div className="col-md-12 pt-3">
-        <div className="row">
-            <div className="col-md-2 mb-3 text-center">
-                <img src={props.image} alt={props.name} className="img-fluid" />
-            </div>
-        </div>
+    <div className="col-md-2 mb-3 text-center mb-md-0 mb-3">
+        <img src={props.image} alt={props.name} className="img-fluid" />
     </div>
 )
 
--- src/data/client-images.js
+++ src/data/client-images.js
@@ -1,10 +1,16 @@
 import ApmTerminals from '../images/apm-terminals.svg'
+import MtnImage from '../images/mtn.svg'
 
 const ClientImages = [
-     {
-         image: ApmTerminals,
-         name: 'Apm Terminals'
-     }
+    {
+        image: MtnImage,
+        name: 'MTN'
+    },
+    {
+        image: ApmTerminals,
+        name: 'Apm Terminals'
+    }
+
 ]
 
 export default ClientImages
